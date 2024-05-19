9a9c0bad9a8fdc074d26ceed6ef76331d43ed2f5
NexusHiveMind <rodrigo.daniel.pena@gmail.com>
2024-05-19 20:10:35 -0300

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_fahrenheit) {
+  let result = (temp_fahrenheit-32)*5/9;
+  result = Math.round(result * 10) / 10;
+  return result;
+  // return (Math.round(result * 100) / 100).toFixed(2);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_celsius) {
+  let result = temp_celsius * 9/5 + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
