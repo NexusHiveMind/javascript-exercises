a1596667485484a9afd6f2675fcdf634a065172a
NexusHiveMind <rodrigo.daniel.pena@gmail.com>
2024-05-19 18:20:19 -0300

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text = string;
+    text_in_char = string.split("");
+    result = text_in_char.toReversed();
+    result = result.join("");
+    return result;
 };
 
 // Do not edit below this line
