8eb8a44a9075f08c1513b6401d5c50144c936369
NexusHiveMind <rodrigo.daniel.pena@gmail.com>
2024-05-19 19:23:00 -0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(initial_number,final_number) {
+    let minor_number;
+    let larger_number;
+    if (initial_number <0 | final_number <0 | typeof(initial_number+final_number)!= "number"){
+        return "ERROR";
+    };
+    if (initial_number<=final_number){
+        minor_number = initial_number;
+        larger_number = final_number;
+    }else{
+        minor_number = final_number;
+        larger_number = initial_number;
+    };
+    let sum=0;
+    for (let i = minor_number;i<=larger_number;i++){
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
